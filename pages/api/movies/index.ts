c2e3e8232567e357d27e15c0be4a016cb9ae0a89
yellhtetaung <yairhtet248.yha@gmail.com>
2023-03-18 03:44:44 +0630

    Final

---
+++ pages/api/movies/index.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import prisamdb from "@/lib/prismadb";
+import serverAuth from "@/lib/serverAuth";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    return res.status(405).end();
+  }
+
+  try {
+    await serverAuth(req);
+
+    const movies = await prisamdb.movie.findMany();
+
+    return res.status(200).json(movies);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end();
+  }
+}
